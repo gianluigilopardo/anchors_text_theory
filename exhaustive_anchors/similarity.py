b4b8cdb239432dd9f035ec56a3f10cb97ef37794
glopardo <gianluigilopardo@gmail.com>
2022-05-23 18:06:57 +0200

    Jaccard similarity between exhaustive Anchors and official implementation

--- exhaustive_anchors/similarity.py
+++ exhaustive_anchors/similarity.py
@@ -9,6 +9,7 @@ from anchor import anchor_text  # Official Anchors
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import Perceptron
@@ -25,86 +26,61 @@ nlp = spacy.load('en_core_web_sm')  # anchors need it
 
 # DATA
 path = os.getcwd().replace('exhaustive_anchors', 'dataset')
-data_yelp = Dataset('yelp', path)
-X_train_yelp, X_test_yelp, y_train_yelp, y_test_yelp = train_test_split(data_yelp.X, data_yelp.y)
-data_restaurants = Dataset('restaurants', path)
-X_train_restaurants, X_test_restaurants, y_train_restaurants, y_test_restaurants = train_test_split(data_restaurants.X, data_restaurants.y)
+datasets = ['restaurants', 'yelp']
+similarity = {}
 
-# classes of the data
-class_names = ["Dislike", "Like"]
+for dataset in datasets:
 
-# (non-normalized) TF-IDF
-vectorizer_yelp = TfidfVectorizer(norm=None)
-vectorizer_restaurants = TfidfVectorizer(norm=None)
-vectors_yelp = vectorizer_yelp.fit_transform(X_train_yelp)
-vectors_restaurants = vectorizer_restaurants.fit_transform(X_train_restaurants)
+    data = Dataset(dataset, path)
+    X_train, X_test, y_train, y_test = train_test_split(data.X, data.y)
 
-# MODELS
-# model returning 1 if 'good' is present
-indicator_yelp = models.Product(vectorizer_yelp, ['good'])
-indicator_restaurants = models.Product(vectorizer_restaurants, ['good'])
-# model returning 1 if 'not' and 'bad are present or 'good' is present
-dtree_yelp = models.DTree(vectorizer_yelp, [['not', 'bad'], ['good']])
-dtree_restaurants = models.DTree(vectorizer_restaurants, [['not', 'bad'], ['good']])
-# logistic model
-logistic_yelp = LogisticRegression().fit(vectors_yelp, y_train_yelp)
-logistic_restaurants = LogisticRegression().fit(vectors_restaurants, y_train_restaurants)
-# perceptron
-perceptron_yelp = Perceptron().fit(vectors_yelp, y_train_yelp)
-perceptron_restaurants = Perceptron().fit(vectors_restaurants, y_train_restaurants)
-# random forest classifier
-rf_yelp = RandomForestClassifier().fit(vectors_yelp, y_train_yelp)
-rf_restaurants = RandomForestClassifier().fit(vectors_restaurants, y_train_restaurants)
+    # classes of the data
+    class_names = ["Dislike", "Like"]
 
+    # (non-normalized) TF-IDF
+    vectorizer = TfidfVectorizer(norm=None)
+    vectors = vectorizer.fit_transform(X_train)
 
-# Jaccard similarity for Yelp
-similarity_yelp = {'Indicator': 0, 'DTree': 0, 'LogisticRegression': 0, 'Perceptron': 0, 'RandomForestClassifier': 0}
-models_yelp = {'Indicator': indicator_yelp, 'DTree': dtree_yelp, 'LogisticRegression': logistic_yelp, 'Perceptron': perceptron_yelp, 'RandomForestClassifier': rf_yelp}
-for k, model in models_yelp.items():
-    # initialize the explainers
-    exhaustive_explainer = empirical_anchor_text.AnchorText(model.predict)
-    official_explainer = anchor_text.AnchorText(nlp, class_names, use_unk_distribution=True)
-    X = np.asarray(X_test_yelp)[model.predict(X_test_yelp) == 1]
-    for i, doc in enumerate(X):
-        print('\n {} / {}'.format((i+1), len(X)))
-        print(doc)
-        prediction = model.predict([doc])
-        print('{} predicts {}'.format(k, str(prediction)))
-        exhaustive_anchor = exhaustive_explainer.explain_instance(doc)
-        official_anchor = official_explainer.explain_instance(str(doc), model.predict).names()
-        current_similarity = jaccard_similarity(official_anchor, exhaustive_anchor)
-        similarity_yelp[k] = similarity_yelp[k] + current_similarity
-        print('Official anchor: {} \nExhaustive anchor: {} \nSimilarity: {} \nOverall similarity: {}'.format(
-            official_anchor, exhaustive_anchor, current_similarity, similarity_yelp))
-    similarity_yelp[k] = similarity_yelp[k] / len(X)
+    # MODELS
+    # model returning 1 if 'good' is present
+    indicator = models.Product(vectorizer, ['good'])
+    # model returning 1 if 'not' and 'bad are present or 'good' is present
+    dtree = models.DTree(vectorizer, [['not', 'bad'], ['good']])
+    # logistic model
+    logistic = make_pipeline(vectorizer, LogisticRegression().fit(vectors, y_train))
+    # perceptron
+    perceptron = make_pipeline(vectorizer, Perceptron().fit(vectors, y_train))
+    # random forest classifier
+    rf = make_pipeline(vectorizer, RandomForestClassifier().fit(vectors, y_train))
+    models_data = {'Indicator': indicator, 'DTree': dtree, 'LogisticRegression': logistic, 'Perceptron': perceptron, 'RandomForestClassifier': rf}
 
-# Jaccard similarity for Restaurants
-similarity_restaurants = {'Indicator': 0, 'DTree': 0, 'LogisticRegression': 0, 'Perceptron': 0, 'RandomForestClassifier': 0}
-models_restaurants = {'Indicator': indicator_restaurants, 'DTree': dtree_restaurants, 'LogisticRegression': logistic_restaurants,
-               'Perceptron': perceptron_restaurants, 'RandomForestClassifier': rf_restaurants}
-for k, model in models_restaurants.items():
-    # initialize the explainers
-    exhaustive_explainer = empirical_anchor_text.AnchorText(model)
-    official_explainer = anchor_text.AnchorText(nlp, class_names, use_unk_distribution=True)
-    X = np.asarray(X_test_restaurants)[model.predict(X_test_restaurants)]
-    for i, doc in enumerate(X):
-        print('\n {} / {}'.format((i + 1), len(X)))
-        print(doc)
-        prediction = model.predict([doc])
-        print('{} predicts {}'.format(k, prediction))
-        exhaustive_anchor = exhaustive_explainer.explain_instance(doc)
-        official_anchor = official_explainer.explain_instance(str(doc), model.predict).names()
-        current_similarity = jaccard_similarity(official_anchor, exhaustive_anchor)
-        similarity_restaurants[k] = similarity_restaurants[k] + current_similarity
-        print('Official anchor: {} \nExhaustive anchor: {} \nSimilarity: {} \nOverall similarity: {}'.format(
-            official_anchor, exhaustive_anchor, current_similarity, similarity_restaurants))
-    similarity_restaurants[k] = similarity_restaurants[k] / len(X)
+    # Jaccard similarity
+    data_similarity = {'Indicator': 0, 'DTree': 0, 'LogisticRegression': 0, 'Perceptron': 0, 'RandomForestClassifier': 0}
+    for k, model in models_data.items():
+        # initialize the explainers
+        exhaustive_explainer = empirical_anchor_text.AnchorText(model.predict)
+        official_explainer = anchor_text.AnchorText(nlp, class_names, use_unk_distribution=True)
+        # We explain posite predictions
+        X = np.asarray(X_test)[model.predict(X_test) == 1]
+        for i, doc in enumerate(X):
+            print('\n {} / {} of {}'.format((i+1), len(X), dataset))
+            print(doc)
+            prediction = model.predict([doc])
+            print('{} predicts {}'.format(k, str(prediction)))
+            exhaustive_anchor = exhaustive_explainer.explain_instance(doc)
+            official_anchor = official_explainer.explain_instance(str(doc), model.predict).names()
+            current_similarity = jaccard_similarity(official_anchor, exhaustive_anchor)
+            data_similarity[k] = data_similarity[k] + current_similarity
+            print('Official anchor: {} \nExhaustive anchor: {} \nSimilarity: {}'.format(
+                official_anchor, exhaustive_anchor, current_similarity))
+        data_similarity[k] = data_similarity[k] / len(X)
+    similarity[dataset] = data_similarity
 
+results = pd.DataFrame(columns=list(models_data.keys()), index=datasets)
 
-results = pd.DataFrame(columns=list(models_yelp.keys()), index=['Yelp', 'Restaurants'])
-for k, model in models_yelp.items():
-    results.loc['Yelp'][k] = similarity_yelp[k]
-    results.loc['Restaurants'][k] = similarity_restaurants[k]
+for dataset in datasets:
+    for k, model in models_data.items():
+        results.loc[dataset][k] = similarity[dataset][k]
 
 print(results)
 pickle.dump(results, open('similarity.p', 'wb'))
