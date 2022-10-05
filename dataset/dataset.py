99bd409e5416a7434128123c7ee78722e57602a5
Gianluigi Lopardo <gianluigilopardo@gmail.com>
2022-10-05 15:26:29 +0200

    added imdb

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,17 +1,17 @@
-"""
-Dataset manager
-"""
-
+# dataset manager
 import pandas as pd
-import os
+
 import re
 
-datasets = ['restaurants', 'yelp']
+import os
+
+datasets = ['restaurants', 'yelp', 'imdb']
 
 
 class Dataset:
     def __init__(self, name, path):
-        assert name in datasets, 'DATASET {} not available! Choose among {}'.format(name, datasets)
+        assert name in datasets, 'DATASET ' + str(name) + ' not available! Chose among ' + str(datasets)
+        # light preprocessing: remove symbols, keep spaces, lowercase
         if name == 'restaurants':
             dataset = 'restaurants.tsv'
             self.df = pd.read_csv(os.path.join(path, dataset), sep='\t')
@@ -23,6 +23,13 @@ class Dataset:
             self.X = [self.preprocess(x) for x in list(self.df["text"])]
             self.y = list(self.df["stars"])
 
+        elif name == 'imdb':
+            dataset = 'imdb.csv'
+            self.df = pd.read_csv(os.path.join(path, dataset), sep=',')
+            self.X = [self.preprocess(x) for x in list(self.df["review"])]
+            self.y = self.df.sentiment.copy()
+            self.y = self.y.replace('positive', 1)
+            self.y = self.y.replace('negative', 0)
+
     def preprocess(self, x):
         return re.sub('[^a-zA-Z\d\s]', '', x).lower()
-
